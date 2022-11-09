0258e24140033bf6a63e70726e0e12f662b89efb
I Fathurohman <faturahman.ilham@gmail.com>
2022-11-09 14:22:18 +0700

    testqa

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const {By, Key, Builder, until } = require('selenium-webdriver');
+const {By, Key, Builder, until, logging } = require('selenium-webdriver');
+
 require('chromedriver');
 
 var URL = "https://todo-devcode.web.app/";
@@ -33,7 +34,7 @@ var URL = "https://todo-devcode.web.app/";
     let activity = await driver.findElement(By.xpath(`//div[@class="jss1"][1]/div/h1[@data-cy="activity-title"]`)).getText();
     // tampil activity
     console.log(activity);
-    // click button activity
+    // click tambah button activity
     await driver.findElement(By.xpath(`//div[@class="jss1"][1]//button[@data-cy="activity-add-button"]`)).click();
     process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
       var options1 = {
@@ -42,22 +43,57 @@ var URL = "https://todo-devcode.web.app/";
         'headers' : {},
       };
       request(options1, function (error, response) {
-        if (error) throw new Error(error);
-        data1            = JSON.parse(response.body);
-        total_activity2  = data1.total;
-        total            = total_activity2 >= total_activity1;
-        if(total === true){
-          console.log('New Activity Success');
-        }else{
-          console.log('New Activity Failed');
-        }
-        console.log(data1.data[0].title);
-        let d  = new Date(data1.data[0].created_at);
-        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-        let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
-        let da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);
-        console.log(`${da} ${mo} ${ye}`);
-      });
+      if (error) throw new Error(error);
+      data1            = JSON.parse(response.body);
+      total_activity2  = data1.total;
+      total            = total_activity2 >= total_activity1;
+      console.log(total_activity2);
+      if(total === true){
+        console.log('New Activity Success');
+      }else{
+        console.log('New Activity Failed');
+      }
+      console.log(data1.data[0].title);
+      let d  = new Date(data1.data[0].created_at);
+      let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+      let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+      let da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(d);
+      console.log(`${da} ${mo} ${ye}`);
+    });
+    // click button batal hapus activity
+    await driver.findElement(By.xpath(`//div[@id='root']//div[1]//div[2]//*[name()='svg'][@data-cy="activity-item-delete-button"]`)).click();
+    await driver.findElement(By.xpath(`//button[@class='jss11 jss20'][@data-cy="modal-delete-cancel-button"]`)).click();
+    // click diluar modal batal hapus activity
+    // await driver.findElement(By.xpath(`//div[@id='root']//div[1]//div[2]//*[name()='svg'][@data-cy="activity-item-delete-button"]`)).click();
+    // await driver.findElement(By.xpath("//html")).click();
+    // // click button hapus activity
+    await driver.findElement(By.xpath(`//div[@id='root']//div[1]//div[2]//*[name()='svg'][@data-cy="activity-item-delete-button"]`)).click();
+    await driver.findElement(By.xpath(`//div[@data-cy="modal-delete"]/div[@class="jss25"]/button[@data-cy="modal-delete-confirm-button"]`)).click();
+    await driver.sleep(3000);
+    let information = await driver.findElement(By.xpath(`//div[@class="jss5"]/div[@data-cy="modal-information"]/div[@data-cy="modal-information-title"]`)).getText();
+    console.log(information);    
+    // cek jumlah activity
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
+      var options2 = {
+        'method'  : 'GET',
+        'url'     : 'https://todo.api.devcode.gethired.id/activity-groups?email=n.latifahulfah@gmail.com',
+        'headers' : {},
+      };
+      request(options2, function (error, response) {
+      if (error) throw new Error(error);
+      data2             = JSON.parse(response.body);
+      total_activity3   = data2.total;
+      total1            = total_activity3 === total_activity1;
+      console.log(total_activity3);
+      if(total1 === true){
+        console.log('Jumlah activity sesuai');
+      }else{
+        console.log('Jumlah activity tidak sesuai');
+      }
+      // console.log(data1.data[0].title);
+    });
   }
   driver.quit();
 })();
+
+
